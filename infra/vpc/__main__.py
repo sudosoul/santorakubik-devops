1c77eebd48f9b1dee57d4a7a2a7284b6e054fa21
Rob Mullins <r.mullins@controlexpertus.com>
2023-06-04 23:58:31 -0400

    created infra/vpc

---
+++ infra/vpc/__main__.py
@@ -0,0 +1,32 @@
+"""An AWS Python Pulumi program"""
+import pulumi
+import pulumi_aws as aws
+import pulumi_awsx as awsx
+
+config = pulumi.Config()
+vpc_name = config.require("vpc_name")
+number_of_availability_zones = config.require_int("number_of_availability_zones")
+vpc_cidr = config.require("vpc_cidr")
+
+# Make sure we are deploying to John's AWS account
+if aws.get_caller_identity().account_id != "927123100668":
+    print(f"ERROR: IN WRONG AWS ACCOUNT!\nCurrent ID:{current.account_id}")
+    exit() 
+
+# Create VPC with following specs:
+# 3 public subnets
+# 3 private subnets
+# 3 NAT Gateways each assigned a dedicated EIP
+vpc = awsx.ec2.Vpc(vpc_name, 
+  cidr_block = vpc_cidr,
+  number_of_availability_zones = number_of_availability_zones,
+  tags = {
+      "Name": "main",
+      "managed_by": "pulumi"
+  }
+)
+
+pulumi.export("vpc_id", vpc.id)
+pulumi.export("outbound_ips", [vpc.eips[i].public_ip for i in range(number_of_availability_zones)])
+pulumi.export("public_subnet_ids", vpc.public_subnet_ids)
+pulumi.export("private_subnet_ids", vpc.private_subnet_ids)
