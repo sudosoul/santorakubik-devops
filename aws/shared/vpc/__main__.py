2a74a77cfa0dc3e937ee220d79e73f95dbe07757
Rob Mullins <r.mullins@controlexpertus.com>
2023-07-06 22:51:20 -0400

    added required tags to subnets for discovery by aws-lb-controller

--- aws/shared/vpc/__main__.py
+++ aws/shared/vpc/__main__.py
@@ -8,9 +8,10 @@ number_of_availability_zones = config.require_int("number_of_availability_zones"
 vpc_cidr = config.require("vpc_cidr")
 
 # Make sure we are deploying to Santorakubik:
+if pulumi.get_organization() != "santorakubik":
+    raise Exception(f"ERROR: IN WRONG PULUMI ORG!\nCurrent ORG:{pulumi.get_organization()}")
 if aws.get_caller_identity().account_id != "927123100668":
-    print(f"ERROR: IN WRONG AWS ACCOUNT!\nCurrent ID:{aws.get_caller_identity().account_id}")
-    exit() 
+    raise Exception(f"ERROR: IN WRONG AWS ACCOUNT!\nCurrent ID:{aws.get_caller_identity().account_id}")
 
 # Create VPC with following specs:
 # 2 public subnets / 2 private subnets
@@ -21,10 +22,30 @@ vpc = awsx.ec2.Vpc(vpc_name,
   tags = {
       "Name": vpc_name,
       "managed_by": "pulumi"
-  }
+  },
+  opts=pulumi.ResourceOptions(ignore_changes=["tags"])
+)
+
+# Add k8s tags to subnets
+vpc.public_subnet_ids.apply(lambda x: \
+  [aws.ec2.Tag(\
+      f"{i}-elb-tag", \
+      resource_id=i, \
+      key='kubernetes.io/role/elb', \
+      value='1')\
+  for i in x]\
+)
+vpc.private_subnet_ids.apply(lambda x: \
+  [aws.ec2.Tag(\
+      f"{i}-elb-tag", \
+      resource_id=i, \
+      key='kubernetes.io/role/internal-elb', \
+      value='1')\
+  for i in x]\
 )
 
 pulumi.export("vpc_id", vpc.vpc_id)
 pulumi.export("outbound_ips", [vpc.eips[i].public_ip for i in range(number_of_availability_zones)])
 pulumi.export("public_subnet_ids", vpc.public_subnet_ids)
 pulumi.export("private_subnet_ids", vpc.private_subnet_ids)
+
